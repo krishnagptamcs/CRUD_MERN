8eb97103a9f26a9a6fc391e279685afa2a5117a8
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2023-07-27 18:35:23 +0530

    some little styling add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -67,7 +67,7 @@ function App() {
 
   return (
     <>
-      <div className="h-screen w-screen bg-black">
+      <div className="min-h-screen w-screen bg-black">
         <section className=" bg-black">
           <div className="grid grid-cols-1 lg:grid-cols-2">
             <div className="relative flex items-end px-4 pb-10 pt-60 sm:px-6 sm:pb-16 md:justify-center lg:px-8 lg:pb-24">
